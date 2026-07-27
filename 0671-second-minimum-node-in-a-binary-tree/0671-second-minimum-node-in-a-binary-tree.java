f1bbdddae64e84ae2ae8bf64480504aeac50fdb9
Priya kumari <PRIYASHAH1539@GMAIL.COM>
2026-07-27 18:32:21 +0530

    Time: 0 ms (100%), Space: 42.6 MB (81.23%) - LeetHub

--- 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.java
+++ 0671-second-minimum-node-in-a-binary-tree/0671-second-minimum-node-in-a-binary-tree.java
@@ -14,34 +14,30 @@
  * }
  */
 class Solution {
+    long min=Long.MAX_VALUE;
+    long secmin= Long.MAX_VALUE;
+    public  void dfs(TreeNode node){
+        if(node==null){
+            return ;
+        }
+        if(node.val<min){
+            secmin=min;
+            min=node.val;
+        }else if(node.val<secmin&& node.val!=min){
+            secmin=node.val;
+        }
+        dfs(node.left);
+        dfs(node.right);
+    }
     public int findSecondMinimumValue(TreeNode root) {
-        if (root == null) {
-            return -1;
-        }
-        List<Integer> s = check(root);
-       Collections.sort(s);
-        int min = s.get(0);
-        long secmin = Long.MAX_VALUE;
-        for (int i = 0; i < s.size(); i++) {
-            if (min < s.get(i)) {
-                secmin = Math.min(secmin, s.get(i));
-            }
-
-        }
-        if (secmin == Long.MAX_VALUE) {
+        
+        dfs(root);
+        if(secmin==Long.MAX_VALUE){
             return -1;
         }
         return (int)secmin;
+        
     }
 
-    public static List<Integer> check(TreeNode node) {
-        if (node == null) {
-            return new ArrayList<>();
-        }
-        List<Integer> l = new ArrayList<>();
-        l.addAll(check(node.left));
-        l.add(node.val);
-        l.addAll(check(node.right));
-        return l;
-    }
+
 }
