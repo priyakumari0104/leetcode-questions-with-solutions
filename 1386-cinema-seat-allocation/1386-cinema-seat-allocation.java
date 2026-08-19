41b89d5cda3e052e5cf201f2546e85df34693573
Priya kumari <PRIYASHAH1539@GMAIL.COM>
2026-08-19 22:13:38 +0530

    Time: 39 ms (5.53%), Space: 56.5 MB (18.16%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -1,26 +1,26 @@
 class Solution {
     public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
-        HashMap<Integer,Set<Integer>> map=new HashMap<>();
-        for(int set[]:reservedSeats){
-            int row=set[0];
-            int seats=set[1];
-            map.putIfAbsent(row,new HashSet<>());
-            map.get(row).add(seats);
-        }
-        int ans=(n-map.size())*2;
-        for(int key:map.keySet()){
-               Set<Integer> reserved= map.get(key);
-               boolean leftavail=!reserved.contains(2)&&!reserved.contains(3)&&!reserved.contains(4)&&!reserved.contains(5);
-               boolean rightavail=!reserved.contains(6)&&!reserved.contains(7)&&!reserved.contains(8)&&!reserved.contains(9);
-               boolean middleavail=!reserved.contains(4)&&!reserved.contains(5)&&!reserved.contains(6)&&!reserved.contains(7);
-               if(leftavail==true&&rightavail==true){
-                ans+=2;
-               }
-                else if(rightavail==true||leftavail==true||middleavail==true){
-                ans+=1;
-               }
+      HashMap<Integer,Set<Integer>> map=new HashMap<>();
+      for(int box[]:reservedSeats){
+        int row=box[0];
+        int seats=box[1];
+        map.putIfAbsent(row,new HashSet<>());
+        map.get(row).add(seats);
+      }
+      int ans=(n-map.size())*2;
 
+      for(int keys: map.keySet()){
+        Set<Integer>reserved=map.get(keys);
+        boolean leftavail=!reserved.contains(2)&&!reserved.contains(3)&&!reserved.contains(4)&&!reserved.contains(5);
+         boolean rightavail=!reserved.contains(6)&&!reserved.contains(7)&&!reserved.contains(8)&!reserved.contains(9);
+        boolean middleavail=!reserved.contains(4)&&!reserved.contains(5)&&!reserved.contains(6)&&!reserved.contains(7);
+        if(leftavail==true&&rightavail==true){
+            ans+=2;
+        }else if(leftavail==true||rightavail==true||middleavail==true){
+            ans+=1;
         }
+      }
+
         return ans;
     }
 }
