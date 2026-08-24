239a6e5133e73781ed3d4631973e051df9cdfcee
Priya kumari <PRIYASHAH1539@GMAIL.COM>
2026-08-24 18:40:00 +0530

    Time: 7 ms (79.86%), Space: 47.3 MB (10.79%) - LeetHub

--- 1927-sum-game/1927-sum-game.java
+++ 1927-sum-game/1927-sum-game.java
@@ -1,38 +1,31 @@
 class Solution {
     public boolean sumGame(String num) {
 
-        int n = num.length();
-        int half = n / 2;
-
-        int leftSum = 0;
-        int rightSum = 0;
-
-        int leftQ = 0;
-        int rightQ = 0;
-
-        for (int i = 0; i < half; i++) {
-            if (num.charAt(i) == '?') {
-                leftQ++;
-            } else {
-                leftSum += num.charAt(i) - '0';
-            }
+       int n=num.length();
+       int half=n/2;
+       int leftsum=0;
+       int rightsum=0;
+       int leftq=0;
+       int rightq=0;
+       for(int i=0;i<half;i++){
+        char ch= num.charAt(i);
+        if(ch=='?'){
+            leftq++;
+        }else{
+            leftsum+=ch-'0';
         }
-
-        for (int i = half; i < n; i++) {
-            if (num.charAt(i) == '?') {
-                rightQ++;
-            } else {
-                rightSum += num.charAt(i) - '0';
-            }
+       }
+       for(int i=half;i<n;i++){
+        char ch=num.charAt(i);
+        if(ch=='?'){
+            rightq++;
+        }else{
+            rightsum+=ch-'0';
         }
-
-        // Odd number of ? -> Alice wins
-        if ((leftQ + rightQ) % 2 == 1) {
-            return true;
-        }
-
-        // Bob can win only if the current difference
-        // can be exactly compensated.
-        return leftSum - rightSum != 9 * (rightQ - leftQ) / 2;
+       }
+       if((leftq+rightq)%2==1){
+        return true;
+       }
+       return rightsum-leftsum!=9*(leftq-rightq)/2;
     }
 }
