4931b755a4b0fb62d10e8e7c05edb22bd2ee689b
Priya kumari <PRIYASHAH1539@GMAIL.COM>
2026-09-05 23:50:55 +0530

    Time: 5 ms (13.15%), Space: 43.1 MB (85.46%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,32 +1,39 @@
 class Solution {
-    public boolean check(char a, char b) {
-        if (a == '(' && b == ')')
-            return true;
-        if (a == '[' && b == ']')
-            return true;
-        if (a == '{' && b == '}')
-            return true;
-        return false;
+ public boolean check(char a,char b){
+    if(a=='('&&b==')'){
+        return true;
+    }else if(a=='['&& b==']'){
+        return true;
+    }else if(a=='{'&&b=='}'){
+        return true;
+    }
+    return false;
+ }
+    public boolean isValid(String s) {
+ int n=s.length();
+ if(n%2!=0){
+    return false;
+ }
+ Stack<Character> st= new Stack<>();
+ for(int i=0;i<n;i++){
+    char ch=s.charAt(i);
+    if(ch=='('||ch=='['||ch=='{'){
+      st.push(ch);
+    }else{
+        if(st.size()==0){
+            return false;
+        }else{
+            char top=st.peek();
+            if(check(top,ch)){
+                st.pop();
+            }else{
+                return false;
+            }
+        }
     }
 
-    public boolean isValid(String s) {
-        if(s.length()%2!=0){return false;}
-        Stack<Character> temp=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(ch=='('||ch=='{'||ch=='['){
-                temp.push(ch);
-            }else{
-                if(temp.isEmpty())
-                return false;
-            
-                char top=temp.peek();
-                if(check(top,ch)){temp.pop();}
-                else{return false;}
-            
-                }
+ }
 
-        }
-        return (temp.isEmpty());
+ return (st.isEmpty());
         }
 }
