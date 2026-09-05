76f7090da071f49f42fad781baabfd21ebbc95d0
Priya kumari <PRIYASHAH1539@GMAIL.COM>
2026-09-05 12:00:10 +0530

    Time: 280 ms (5.24%), Space: 47.8 MB (57.31%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,16 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int n=nums.length;
-        int j=0;
         for(int i=0;i<n;i++){
-            if(nums[i]!=0){
-                nums[j]=nums[i];
-                j++;
+            for(int j=0;j<n-1-i;j++){
+                if(nums[j]==0&&nums[j+1]!=0){
+                    int temp=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+                }
             }
-        }
-        while(j<n){
-            nums[j]=0;
-            j++;
         }
     }
 }
