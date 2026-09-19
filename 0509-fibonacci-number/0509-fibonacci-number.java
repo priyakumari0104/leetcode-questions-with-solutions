dfc793351547e0195963c9b8c851919c5c4614d0
Priya kumari <PRIYASHAH1539@GMAIL.COM>
2026-09-19 11:17:56 +0530

    Time: 8 ms (43.59%), Space: 41.7 MB (96.62%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,19 +1,10 @@
 class Solution {
     
     public int fib(int n) {
-       int dp[]=new int [n+1];
-        return fibo(n,dp);
+    if(n==0||n==1){
+        return n;
     }
-    public int fibo(int n,int dp[]){
-        if(n<=1){
-            return n;
-        }
-        if(dp[n]!=0){
-            return dp[n];
-        }
-        int ans= fibo(n-1,dp)+fibo(n-2,dp);
-        dp[n]=ans;
-        return ans;
+    return fib(n-1)+fib(n-2);
     }
    
 }
