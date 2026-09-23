212b9976d98c8cc1a15e2bc9e66580186777d43e
Priya kumari <PRIYASHAH1539@GMAIL.COM>
2026-09-24 00:43:38 +0530

    Time: 0 ms (100%), Space: 48.6 MB (70.34%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n=nums.length;
+       if(n<=2){
+        return n;
+       }
+       int slow=2;
+       int fast=2;
+       while(fast<n){
+        if(nums[fast]!=nums[slow-2]){
+            nums[slow]=nums[fast];
+            slow++;        }
+            fast++;
+       }
+       return slow;
+    }
+
+}
